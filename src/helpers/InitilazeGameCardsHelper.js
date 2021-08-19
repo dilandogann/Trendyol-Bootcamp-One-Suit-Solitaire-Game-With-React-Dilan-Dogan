739557ff574f23fbd0dae62bac3de63cdfde724f
unknown <d.dogan@etu.edu.tr>
2021-08-19 21:27:30 +0300

    Inıtializing first card array

---
+++ src/helpers/InitilazeGameCardsHelper.js
@@ -0,0 +1,25 @@
+import Card from '../Card';
+import CardValues from '../CardValues';
+
+let id = 1;
+let cards = [];
+let playingCards = [];
+let floorCards = [];
+
+//It creates 8 copy of each playing cards
+export const initializeCards = () => {
+  for (let i = 0; i < 8; i++) {
+    createPlayingCard();
+  }
+  return { playingCards, floorCards };
+
+};
+
+//It creates 13 different playing cards
+const createPlayingCard = () => {
+  const initialCards = CardValues.map((cardItem) => {
+    return new Card(cardItem.value, cardItem.nextValue,cardItem.image, id++);
+  });
+
+  cards.push(...initialCards);
+};

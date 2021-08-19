2dcb64363a983abd66b768dc351119d8c1f19ea7
unknown <d.dogan@etu.edu.tr>
2021-08-19 21:46:27 +0300

    initializing playing cards and floor cards

--- src/components/PlayGround.js
+++ src/components/PlayGround.js
@@ -1,10 +1,20 @@
 import { Container, Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { initializeCards } from '../game/useLogic';
 import Timer from './Timer';
 import ScoreBoard from './ScoreBoard';
 
 
 const PlayGround = () => {
+
+    const [cards, setCards] = useState([]);
+    const [floorCards, setFloorCards] = useState([]);
+
+    useEffect(() => {
+        const { playingCards, floorCards } = initializeCards();
+        setCards(playingCards);
+        setFloorCards(floorCards);
+      }, []);
 
   return (
     <Container maxWidth='lg'>

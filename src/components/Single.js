68a76b8b6051055d906e02c6ecdff34778f9219b
unknown <d.dogan@etu.edu.tr>
2021-08-20 16:19:49 +0300

    React dnd added for dragging items

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import spadesBack from '../assets/spades-back.png';
 import { makeStyles } from '@material-ui/core';
+import { useDrag } from 'react-dnd';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -45,15 +46,26 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Single = ({ card, click, chunkIndex, cardIndex }) => {
+const Single = ({ card, click, chunkIndex, cardIndex , isDraggable}) => {
   const classes = useStyles(cardIndex);
 
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'image',
+    item: { card, chunkIndex, cardIndex },
+    canDrag: () => isDraggable(),
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  
   return card.showFront ? (
     <img
+      ref={drag}
       src={card.image}
       alt={card.image}
       className={classes.showFront}
       onClick={click}
+      style={{ border: isDragging ? '3px solid pink' : '0px' }}
     />
   ) : (
     <img

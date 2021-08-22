cfec3f7a4d4364ae1490817857fb10570e82a7e6
unknown <d.dogan@etu.edu.tr>
2021-08-22 23:26:41 +0300

    Bug fix

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,12 +1,12 @@
 import { Typography } from '@material-ui/core';
-import React, { forwardRef, useContext } from 'react';
+import React, {useContext } from 'react';
 import { ScoreContext } from '../context/ScoreContext';
 
 const ScoreBoard = () => {
 
   const { score} = useContext(ScoreContext)
   return (
-    <Typography variant='h5' display='inline' is="score-text">
+    <Typography variant='h5' display='inline'>
       Score:
       <Typography variant='body1' style={{fontWeight: 'bold',fontSize:24}} display='inline' color='secondary'>
         {' '}
